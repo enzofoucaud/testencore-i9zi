857289a4d366a6f7aeb2752b978dd458042640d9
foucaud_e <efoucaud@ifotec.com>
2022-06-07 14:43:25 +0200

    New: Bye func

--- hello/hello.go
+++ hello/hello.go
@@ -10,6 +10,12 @@ func World(ctx context.Context, name string) (*Response, error) {
 	return &Response{Message: msg}, nil
 }
 
+//encore:api public path=/bye/:name
+func Bye(ctx context.Context, name string) (*Response, error) {
+	msg := "Bye, " + name + "!"
+	return &Response{Message: msg}, nil
+}
+
 type Response struct {
 	Message string
 }
